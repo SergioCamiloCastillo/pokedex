0e85dc669be1cdc74ad56f3e3cbec5c2ff71876d
scastillonu <102160857+scastillonu@users.noreply.github.com>
2022-04-02 20:41:17 -0500

    login/logout

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import Navigation from "./src/navigation/Navigation";
+import { AuthProvider } from "./src/context/AuthContext";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Navigation />
+      <AuthProvider>
+       
+        <Navigation />
+      </AuthProvider>
     </NavigationContainer>
   );
 }
---
+++ src/utils/userDb.js
@@ -0,0 +1,10 @@
+export const user = {
+  username: "Sergiocas",
+  password: "1234",
+};
+export const userDetails={
+    username:"scastillonu",
+    firstName:"sergio",
+    lastName:"castillo",
+    email:"sergio@gmail.com"
+}
